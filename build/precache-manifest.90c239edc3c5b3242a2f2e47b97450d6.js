0bbaca5bfc58bd4d93676fc6448f13bc55a5bb0f
Ben DeMaster <Benjamin.DeMaster@ThomsonReuters.com>
2019-03-05 10:50:04 -0600

    adding build folder

---
+++ build/precache-manifest.90c239edc3c5b3242a2f2e47b97450d6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "ae48b2f5b6af6a45afce",
+    "url": "/static/js/main.ae48b2f5.chunk.js"
+  },
+  {
+    "revision": "23508a2d5402f5145dbd",
+    "url": "/static/js/2.23508a2d.chunk.js"
+  },
+  {
+    "revision": "ae48b2f5b6af6a45afce",
+    "url": "/static/css/main.5b2157fb.chunk.css"
+  },
+  {
+    "revision": "ab2711dfe44d9bab8b24b09e998865ad",
+    "url": "/index.html"
+  }
+];
